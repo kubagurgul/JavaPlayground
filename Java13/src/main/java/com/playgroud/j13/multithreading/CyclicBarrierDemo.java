49fa06bfa7a492a0b4ab47bd2a66a37c06cef145
JGurgul <jgurgul@luxoft.com>
2020-09-12 15:03:35 +0200

    CyclicBarrierDemo

---
+++ Java13/src/main/java/com/playgroud/j13/multithreading/CyclicBarrierDemo.java
@@ -0,0 +1,30 @@
+package com.playgroud.j13.multithreading;
+
+import java.util.concurrent.*;
+
+public class CyclicBarrierDemo {
+    public static void main(String[] args) {
+
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3);
+        Runnable runnable = () -> {
+            for (int i = 0; i < 4; i++) {
+                System.out.println(Thread.currentThread().getId() + " : " + i);
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        };
+
+        ExecutorService es = Executors.newFixedThreadPool(3);
+
+        es.submit(runnable);
+        es.submit(runnable);
+        es.submit(runnable);
+
+
+        System.out.println("Done");
+    }
+}
